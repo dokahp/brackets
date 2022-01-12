eea115445e21c2f395e4bf7d3083e9f7c47b619d
Vitaliy Dreko <vitali.dreko@gmail.com>
2022-01-13 00:36:03 +0300

    feat: add JS: Brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let checkArr = []
+  for (bracket of str) {
+    let indexBracketArr = bracketsConfig.map(el => el.includes(bracket)).findIndex(el => el == true)
+    if (checkArr[checkArr.length - 1] == bracketsConfig[indexBracketArr][0] && bracket == bracketsConfig[indexBracketArr][1]){
+      checkArr.pop()
+    } else {
+      checkArr.push(bracket)
+    }
+  }
+  return !checkArr.length
 }
